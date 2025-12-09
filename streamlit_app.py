0eb60e11000138a5d328cfe5a7aa1cc0873fa27d
Yaribar <40440445+Yaribar@users.noreply.github.com>
2025-12-08 22:36:38 -0600

    Fix typo in scatter chart x-axis label

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ with st.expander("Data"):
   y
 
 with st.expander("Data visualization"):
-  st.scatter_chart(data = df,x = 'bill_legth_mm', y = 'body_mass_g', color = 'species')
+  st.scatter_chart(data = df,x = 'bill_length_mm', y = 'body_mass_g', color = 'species')
